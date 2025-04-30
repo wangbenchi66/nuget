91972e5f6c5c2d9a11f72e5a3c75cdf1aab183a3
wangbenchi66 <69945864@qq.com>
2025-04-30 14:55:23 +0800

    json时间格式化

--- src/Easy.Common.Core/JsonHelper.cs
+++ src/Easy.Common.Core/JsonHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Easy.Common.Core
 {
@@ -7,30 +8,6 @@ namespace Easy.Common.Core
     /// </summary>
     public static class JsonHelper
     {
-        /// <summary>
-        /// json序列化
-        /// </summary>
-        /// <param name="obj">要序列化的对象</param>
-        /// <returns>序列化后的json字符串</returns>
-        public static string ToJson(this object obj)
-        {
-            if (obj == null)
-                return string.Empty;
-            return JsonSerializer.Serialize(obj);
-        }
-
-        /// <summary>
-        /// json反序列化
-        /// </summary>
-        /// <typeparam name="T">目标对象类型</typeparam>
-        /// <param name="json">要反序列化的json字符串</param>
-        /// <returns>反序列化后的目标对象</returns>
-        public static T ToObject<T>(this string json)
-        {
-            if (json.IsNull())
-                return default(T);
-            return JsonSerializer.Deserialize<T>(json);
-        }
 
         /// <summary>
         /// json序列化(自定义序列化格式)
@@ -68,12 +45,43 @@ namespace Easy.Common.Core
             return new JsonSerializerOptions
             {
                 //忽略循环引用
-                ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles,
+                ReferenceHandler = ReferenceHandler.IgnoreCycles,
                 //日期格式化
                 WriteIndented = true,
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+                //设置属性命名策略
+                PropertyNamingPolicy = null,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+                //设置编码器
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                //保持原格式
+                AllowTrailingCommas = true,
+                //时间格式化
+                Converters = { new DateTimeConverter("yyyy-MM-dd HH:mm:ss") }
             };
         }
+
+        /// <summary>
+        /// 时间格式化转换器
+        /// </summary>
+        private class DateTimeConverter : JsonConverter<DateTime>
+        {
+            private readonly string _format;
+
+            public DateTimeConverter(string format = "yyyy-MM-dd HH:mm:ss")
+            {
+                _format = format;
+            }
+
+            public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            {
+                return DateTime.Parse(reader.GetString()!);
+            }
+
+            public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+            {
+                writer.WriteStringValue(value.ToString(_format));
+            }
+        }
+
     }
 }
